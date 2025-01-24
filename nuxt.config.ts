18cb00f38a5e22dd94538bfe26e594989cf23983
KiAi <94138724+broccoli88@users.noreply.github.com>
2025-01-24 15:26:25 +0100

    Init + dependencies

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+     compatibilityDate: "2024-11-01",
+     devtools: { enabled: true },
+     css: ["~/assets/css/main.css"],
+
+     vite: {
+          plugins: [tailwindcss()],
+     },
+
+     modules: [
+          "@nuxtjs/supabase",
+          "@nuxt/icon",
+          "@nuxt/image",
+          "@vee-validate/nuxt",
+     ],
+
+     supabase: {
+          url: process.env.SUPABASE_URL,
+          key: process.env.SUPABASE_KEY,
+          redirectOptions: {
+               login: "/admin/login",
+               include: ["/admin(/.*)?"],
+          },
+     },
+});
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+     <div>
+          <h1 class="text-6xl">Test Heading</h1>
+     </div>
+</template>
+
+<style></style>
